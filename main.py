02c2865f85ffa5ab4f133351a3242e3934afb4f6
KesterDaniel <kesterdan17@gmail.com>
2023-01-30 11:24:24 +0100

    write the name of state according to user answer

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import turtle
 import pandas
 from turtle import Screen
 
+timmy = turtle.Turtle()
+timmy.hideturtle()
+
 screen = Screen()
+screen.setup(725, 491)
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
@@ -15,8 +19,12 @@ while game_is_on:
     answer = screen.textinput(title="Guess the state", prompt="What's another state's name?").title()
     states = pandas.read_csv("50_states.csv")
     correct_state = states[states.state == answer]
-    print(correct_state)
-    game_is_on = False
-
+    if not correct_state.empty:
+        xcord = int(correct_state.x)
+        ycord = int(correct_state.y)
+        timmy.penup()
+        timmy.goto(xcord, ycord)
+        timmy.write(answer.title(), align="center")
+        
 
 screen.exitonclick()
