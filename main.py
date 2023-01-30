89812608c4d7984068e99f9cef7439bc4adeeea8
KesterDaniel <kesterdan17@gmail.com>
2023-01-30 11:55:03 +0100

    keeping track of user score, answers and final commit

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-User_Score = 0
 game_is_on = True
+user_answers = []
 
 while game_is_on:
-    answer = screen.textinput(title="Guess the state", prompt="What's another state's name?").title()
+    answer = screen.textinput(title=f"{len(user_answers)}/50 states correct", prompt="What's another state's name?").title()
     states = pandas.read_csv("50_states.csv")
     correct_state = states[states.state == answer]
-    if not correct_state.empty:
+    if not correct_state.empty and answer not in user_answers:
         xcord = int(correct_state.x)
         ycord = int(correct_state.y)
         timmy.penup()
         timmy.goto(xcord, ycord)
-        timmy.write(answer.title(), align="center")
-        
+        timmy.write(answer.title(), align="center", font=('Arial', 8, 'normal'))
+        user_answers.append(answer)
+
+    if len(user_answers) == 50:
+        game_is_on = False
+
 
 screen.exitonclick()
