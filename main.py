14b428f9a6a769d69bc95df92af9a90a2a292345
KesterDaniel <kesterdan17@gmail.com>
2023-01-30 08:42:11 +0100

    loading up image

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import turtle
 from turtle import Screen
 
 screen = Screen()
 screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
 
 
 screen.exitonclick()
