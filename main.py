627d2cd3fbcbdb6221041e07219a7cdfd9a94e2e
KesterDaniel <kesterdan17@gmail.com>
2023-01-30 09:04:53 +0100

    checking user input against csv data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 from turtle import Screen
 
 screen = Screen()
@@ -7,8 +8,15 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer = screen.textinput(title="Guess the state", prompt="What's another state's name?")
-print(answer)
+User_Score = 0
+game_is_on = True
+
+while game_is_on:
+    answer = screen.textinput(title="Guess the state", prompt="What's another state's name?").title()
+    states = pandas.read_csv("50_states.csv")
+    correct_state = states[states.state == answer]
+    print(correct_state)
+    game_is_on = False
 
 
 screen.exitonclick()
