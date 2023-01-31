dd2103988ac820b45d006adbc332601efd72848a
KesterDaniel <kesterdan17@gmail.com>
2023-01-31 10:10:32 +0100

    made some changes

--- main.py
+++ main.py
@@ -15,16 +15,17 @@ turtle.shape(image)
 game_is_on = True
 user_answers = []
 
+states = pandas.read_csv("50_states.csv")
+
 while game_is_on:
     answer = screen.textinput(title=f"{len(user_answers)}/50 states correct", prompt="What's another state's name?").title()
-    states = pandas.read_csv("50_states.csv")
     correct_state = states[states.state == answer]
     if not correct_state.empty and answer not in user_answers:
         xcord = int(correct_state.x)
         ycord = int(correct_state.y)
         timmy.penup()
         timmy.goto(xcord, ycord)
-        timmy.write(answer.title(), align="center", font=('Arial', 8, 'normal'))
+        timmy.write(answer.title(), font=('Arial', 8, 'normal'))
         user_answers.append(answer)
 
     if len(user_answers) == 50:
