2dec03f6e7556a8d8833e69676bdf8bbf1e2f6f7
KesterDaniel <kesterdan17@gmail.com>
2023-01-31 11:11:35 +0100

    saving missed cities to csv files

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ user_answers = []
 states = pandas.read_csv("50_states.csv")
 
 while game_is_on:
-    answer = screen.textinput(title=f"{len(user_answers)}/50 states correct", prompt="What's another state's name?").title()
+    answer = screen.textinput(title=f"{len(user_answers)}/50 states correct", prompt="What's another state's name?") \
+        .title()
+
+    if answer == "Exit":
+        break
     correct_state = states[states.state == answer]
     if not correct_state.empty and answer not in user_answers:
         xcord = int(correct_state.x)
@@ -31,5 +35,27 @@ while game_is_on:
     if len(user_answers) == 50:
         game_is_on = False
 
+states_list = states["state"].to_list()
+missing_states = []
+missing_states_x = []
+missing_states_y = []
 
-screen.exitonclick()
+for state in states_list:
+    if state not in user_answers:
+        missing_state = states[states["state"] == state]
+        missing_state_xcoord = missing_state.x
+        missing_state_xvalue = missing_state_xcoord.item()
+        missing_state_ycoord = missing_state.y
+        missing_state_yvalue = missing_state_ycoord.item()
+        missing_states.append(state)
+        missing_states_x.append(missing_state_xvalue)
+        missing_states_y.append(missing_state_yvalue)
+
+states_to_learn = {
+    "state": missing_states,
+    "x": missing_states_x,
+    "y": missing_states_y
+}
+
+states_to_learn_data = pandas.DataFrame(states_to_learn)
+states_to_learn_data.to_csv("states_to_learn.csv")
